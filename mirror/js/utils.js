1a09e4593c62328ed97710552e72654c2ec26f20
Shilo Mangam <smangam@gmail.com>
2018-03-03 19:11:18 +0200

    utils refactor

--- mirror/js/utils.js
+++ mirror/js/utils.js
@@ -1,28 +1,35 @@
-function simulateClickOn(body, {clientX, clientY}) {
-  const clickDiv = document.createElement('div');
-  clickDiv.classList.add('click-effect');
-  clickDiv.style.top = clientY;
-  clickDiv.style.left = clientX;
-  body.appendChild(clickDiv);
-  clickDiv.addEventListener('animationend', () => {
-    clickDiv.parentNode.removeChild(clickDiv);
-  });
-}
+window.Utils = (function () {
+  function Utils() {}
 
-function createCursorElementOn(body) {
-  const cursor = document.createElement('img');
-  cursor.setAttribute('src', '../assets/cursor.svg');
-  cursor.classList.add('cursor');
-  return body.appendChild(cursor);
-}
-
-function insertCSS() {
-  const cssTag = document.createElement('link');
-  const options = {
-    rel: 'stylesheet',
-    type: 'css/text',
-    href: '../styles/main.css'
+  Utils.prototype.simulateClickOn = function (body, {clientX, clientY}) {
+    const clickDiv = document.createElement('div');
+    clickDiv.classList.add('click-effect');
+    clickDiv.style.top = clientY;
+    clickDiv.style.left = clientX;
+    body.appendChild(clickDiv);
+    clickDiv.addEventListener('animationend', () => {
+      clickDiv.parentNode.removeChild(clickDiv);
+    });
   };
-  Object.assign(cssTag, options);
-  document.querySelector('head').appendChild(cssTag);
-}
+
+  Utils.prototype.createCursorElementOn = function (body) {
+    const cursor = document.createElement('img');
+    cursor.setAttribute('src', '../assets/cursor.svg');
+    cursor.classList.add('cursor');
+    return body.appendChild(cursor);
+  };
+
+  Utils.prototype.insertCSS = function () {
+    const cssTag = document.createElement('link');
+    const options = {
+      rel: 'stylesheet',
+      type: 'css/text',
+      href: '../styles/main.css'
+    };
+    Object.assign(cssTag, options);
+    document.querySelector('head').appendChild(cssTag);
+  };
+
+  return new Utils();
+})();
+
