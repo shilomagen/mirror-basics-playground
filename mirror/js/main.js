3e1c16342caa4049d0853674497e1ece45d50501
Shilo Mangam <smangam@gmail.com>
2018-03-10 12:22:05 +0200

    connecting mirror to ws

--- mirror/js/main.js
+++ mirror/js/main.js
@@ -1,14 +1,27 @@
-const mirror = (function(wsAddr) {
+const mirror = (function (wsAddr) {
   function Mirror(wsAddr) {
     this.wsAddr = wsAddr;
+    this.wsInstance = null;
   }
-  Mirror.prototype.init = function() {
+
+  Mirror.prototype.init = function () {
+    this.connectToServer();
     //Here we're gonna start everything!
+  };
+
+  Mirror.prototype.connectToServer = function () {
+    this.wsInstance = new WebSocket(this.wsAddr);
+    this.wsInstance.onopen = () => console.log('Mirror connected to WS successfully');
+  };
+
+  Mirror.prototype.isWSConnected = function () {
+    return this.wsInstance.readyState === 1;
   };
 
   return new Mirror(wsAddr);
 
 })('ws://localhost:4000/mirror');
+
 
 mirror.init();
 
