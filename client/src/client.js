2228f4a3aa7a6d24263a27b0cc0e77d02ba0c01e
Shilo Mangam <smangam@gmail.com>
2018-03-14 22:52:09 +0200

    dom init change

--- client/src/client.js
+++ client/src/client.js
@@ -2,13 +2,30 @@ import DomTreeSerializer from 'dom-tree-serializer';
 
 export class Client {
   init(wsAddr) {
-    this.connectToServer(wsAddr);
+    this.connectToWS(wsAddr);
     this.initEventHandlers();
   }
 
-  connectToServer(wsAddr) {
+  connectToWS(wsAddr) {
     this.wsInstance = new WebSocket(wsAddr);
     this.wsInstance.onopen = this.onSocketOpen;
+  }
+
+  onSocketOpen = () => {
+    console.log('Client connected to WS successfully');
+    this.dts = new DomTreeSerializer(document, {
+      initialize: this.handleDomInit,
+      applyChanged:() => {
+        console.log('changed!');
+      }
+    });
+
+  }
+
+  sendMsg(msg) {
+    if (this.isWSConnected()) {
+      this.wsInstance.send(JSON.stringify(msg));
+    }
   }
 
   isWSConnected() {
@@ -18,48 +35,40 @@ export class Client {
   initEventHandlers() {
     document.addEventListener('mousemove', this.handleMouseMove);
     document.addEventListener('click', this.handleClick);
-    document.addEventListener('scroll', this.handleScroll);
+    document.addEventListener('scroll', this.handleScroll)
   }
-
-  sendMsg(msg) {
-    if (this.isWSConnected()) {
-      this.wsInstance.send(JSON.stringify(msg));
-    } else {
-      console.log('Websocket state is not OPEN');
-    }
-  }
-
-  onSocketOpen = () => {
-    console.log('Client connected to WS successfully');
-    this.dts = new DomTreeSerializer(document, {
-      initialize: this.handleDomInit,
-      applyChanged: () => {
-        console.log('Changed');
-      }
-    });
-  };
 
   handleMouseMove = ({pageX, pageY}) => {
-    const mouseEvent = {type: 'MOUSE_EVENT', eventData: {pageX, pageY}};
+    const mouseEvent = {
+      type: 'MOUSE_EVENT',
+      eventData: {pageX, pageY}
+    };
     this.sendMsg(mouseEvent);
   };
 
   handleClick = ({clientX, clientY}) => {
-    const clickEvent = {type: 'CLICK_EVENT', eventData: {clientX, clientY}};
+    const clickEvent = {
+      type: 'CLICK_EVENT',
+      eventData: {clientX, clientY}
+    };
     this.sendMsg(clickEvent);
   };
 
   handleScroll = () => {
     const {pageYOffset, pageXOffset} = window;
-    const scrollEvent = {type: 'SCROLL_EVENT', eventData: {pageYOffset, pageXOffset}};
+    const scrollEvent = {
+      type: 'SCROLL_EVENT',
+      eventData: {pageYOffset, pageXOffset}
+    };
     this.sendMsg(scrollEvent);
   };
 
   handleDomInit = (rootId, children) => {
     const domInitEvent = {
-      type: 'DOM_INIT',
+      type:'DOM_INIT_EVENT',
       eventData: {rootId, children}
     };
     this.sendMsg(domInitEvent);
-  };
+  }
+
 }
